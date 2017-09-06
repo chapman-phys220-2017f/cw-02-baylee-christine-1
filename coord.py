96a044a1f56a171bdb82cf9a1fbd90c3f1413e8b
Christine <outla100@mail.chapman.edu>
2017-09-06 06:28:44 +0000

    coord.py updated

--- coord.py
+++ coord.py
@@ -9,31 +9,34 @@ CW 02
 """
 
 # Using a For loop
-def coord_for(n, a, b):
+def coord_for(n,a,b):
     h = float((b-a)/n)
     x =[]
     for i in range(n+1):
-        x.append(a)
-        a = a+h
+        x.append((a+(i*h))
     return x
-coord_for(9,0,1)
-
 
 # Using a While loop
-def coord_while(n, a, b):
+def coord_while(n,a,b):
     h=(b-a)/n
     x=[]
     while a<b:
         x.append(a)
         a = a+h
     return x
-coord_while(9,0,1)
-
-
 
 # Using a List Comprehension
-def coord_comp(n, a, b):
+def coord_comp(n,a,b):
     h=(b-a)/n
     x =[(a+(i*h)) for i in range(n+1)]
     return x
-coord_comp(9,0,1)
+
+# Main block which takes in user input and calls the 3 previously
+# defined functions.
+if __name__ == "__main__":
+    print("Enter 3 values; n, number of intervals, a, starting point, and b, end point.")
+    n,a,b = input("Input in the order and style of n,a,b. ")
+    print("Using a for loop: "+coord_for(n,a,b))
+    print("Using a while loop: "+coord_while(n,a,b))
+    print("Using a list comprehension: "+coord_comp(n,a,b))
+
