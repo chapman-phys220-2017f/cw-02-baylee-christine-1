1f2f6b1c501551ba3fa726d1fda974af62433a25
Christine <outla100@mail.chapman.edu>
2017-09-07 16:51:25 +0000

    converge.py

--- converge.py
+++ converge.py
@@ -9,24 +9,24 @@ CW 02
 """
 
 """
-(b) Show to what value does the sum converges as you let the tolerance become smaller and smaller.
+Function to compute the infinite sum.
 """
 
 def compute_sum(tol):
     k=1
-    con_sum = float(0)
-    while tol<1/(k**2):
-        con_sum+=(k**2)
+    con_sum = 0
+    next_term = 1/(k**2)
+    while tol<next_term:
+        next_term = 1/(k**2)
+        con_sum+=next_term
         k+=1
     return con_sum
 
 """
-Main block calling the function and showing the value to which the sum converges
+Main block calling the function and showing the value to which the sum converges to as tolerance gets smaller
 """
 if __name__ == '__main__':
     convergence = compute_sum(tol=1e-2)
-    print("The sum converges at "+convergence)
-
-    
-    
-# how do you let tolerance become smaller and smaller?
+    print("The sum converges at "+convergence+" when tolerance is set to 1e-2")
+    small_tol_convergence = compute_sum(tol=1e-10)
+    print("The sum converges at "+small_tol_convergence+" when tolerance is set to 1e-10. This can be rounded to 1.645")
