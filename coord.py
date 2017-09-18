d07d7c3fd0559642cb0d31b491a93daa554b3013
Justin Dressel <prof.justin.dressel@gmail.com>
2017-09-18 02:14:34 -0700

    Instructor comments

--- coord.py
+++ coord.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+                        ### python3
 
+### Should be a comment block
 """
 Christine Outlaw
 1912737
@@ -8,6 +10,7 @@ PHYS220
 CW 02
 """
 
+### Should only be one docstring like this at top of file, which describes entire module in detail.
 """
 3 ways to generate equally spaced coordinates with an interval.
 """
@@ -38,12 +41,13 @@ def coord_comp(n,a,b):
     x_list =[(a+(i*h)) for i in range(n+1)]
     return x_list
 
+### NOTE: This should not be a docstring, but a comment
 """
 Main block which takes in user input and calls the 3 previously defined functions.
 """
 if __name__ == "__main__":
     print("Enter 3 values; n, number of intervals, a, starting point, and b, end point.")
-    n = int(input("Input the number of intervals: "))
+    n = int(input("Input the number of intervals: "))                                      ### Never use input()
     a = float(input("Input a start point: "))
     b = float(input("Input the end point: "))
     print("For loop: " + str(coord_for(n,a,b)))
